66ad87cc863e7556655e822aea7885cc09d72c1d
lucadeoliveira2 <lucadeoliveira2@gmail.com>
2024-07-23 09:59:33 +0100

    BS PAGE

--- Black-Scholes.py
+++ Black-Scholes.py
@@ -12,6 +12,22 @@ from matplotlib.colors import LinearSegmentedColormap
 st.title("Option Pricing Web App")
 st.header("Black-Scholes Formula Option Pricing")
 
+def main_page():
+    st.markdown("Black-Scholes")
+    st.sidebar.markdown("Black-Scholes")
+
+def page2():
+    st.markdown("Monte-Carlo Sim")
+    st.sidebar.markdown("Monte-Carlo Sim️")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
+
 # Implementing BS formula
 
 def blackScholes(S, K, T, vol, r):
