20e0affd3d9196be225a8608758a165f40ead2c0
lucadeoliveira2 <lucadeoliveira2@gmail.com>
2024-08-20 14:39:28 +0100

    Small Tweaks

--- pages/Binomial Model Option Pricing.py
+++ pages/Binomial Model Option Pricing.py
@@ -5,8 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Formatting Page
-st.title('Option Pricing Web App')
-st.header('Binomial Model Option Pricing')
+st.title('Binomial Model Option Pricing')
 
 # Getting Option Info From End User
 
@@ -74,12 +73,53 @@ st.subheader(f"Theoretical value of the {optiontype} option is {optionvalue:.3f}
 
 # Plotting Expiration Price
 
-sns.lineplot(x=finalprices, y=finalprobas, color='r')
-plt.title("Probability Distribution of Expiration Price")
-plt.xlabel("Underlying Price ($)")
-plt.ylabel("Probability")
+#sns.lineplot(x=finalprices, y=finalprobas, color='r')
+#plt.title("Probability Distribution of Expiration Price")
+#plt.xlabel("Underlying Price ($)")
+#plt.ylabel("Probability")
 
-st.pyplot()
+#st.pyplot()
+
+# Explaining the Binomial Model
+
+st.header("Comments on the  Binomial Model")
+
+st.markdown("""
+The Binomial model for option pricing is a non-continuous time model that uses principles from the binomial distribution.
+The model makes a number of assumptions including:
+- The underlying price can only move in two particular ways, upwards by factor u or downwards by factor d.
+- The probability of upwards and downwards movement are constant, and price returns are log-normal.
+- The risk-free rate (r) is constant.
+- No arbitrage, such that the expected underlying price through time should equal that of the risk-free return
+- Time is discretised.
+- Options are European.
+
+The no-arbitrage condition implies that there are no risk-free strategies that earn more that the risk-free rate that is pre-determined.
+In an environment where arbitrage exists, traders could open positions that are hedged against price movement risk (delta-neutral) and expect positive returns over time.
+A simple way to profit off this would be to buy in the current market and sell in the futures market  (if the futures market price is inflated), we would then make a risk-free profit by expiration as the prices meet eachother.
+The no-arbitrage condition ensures that options are fairly priced by isolating the role of volatility.
+
+To account for this no-arbitrage condition, we must operate in a risk-neutral probability space.
+This means that we must determine up and down factors, as well as up and down probabilities such that the discounted expected underlying price through time is equal to its current price.
+To do this, we assume a log-normal distribution of returns through time, which leads us to the following equations:
+""")
+st.latex("u  = e^{σΔt^{0.5}}, d = e^{-σΔt^{0.5}}")
+st.latex(r"P(u) = \frac{e^{-rT} - d}{u-d}")
+st.latex("P(d) = 1 - P(u)")
+
+st.write("""
+Where σ = annualized volatility, Δt = timestep, r = risk-free rate and T = time (annualised).
+
+This model finds strength in its ease of application and understanding, providing an efficient way of getting an option price.
+It is not computationally expensive and yields an approximation quickly.
+
+However, it suffers from numerous weaknesses too.
+First, the assumptions around the constant volatility and risk-free rates are dangerous, as they can change through time and significantly affect option prices.
+Next, the simple fact that volatility is assumed makes models in general weak.
+The use of discretised time can also lead to computational inaccuracies.
+The log-normal assumption is unlikely to be true, especially in market events causing fatter tails than expected by such a return distribution.
+The model loses in  accuracy in high volatility and short-time environments.
+Finally, they do not account for transaction costs and liquidity, which may severely affect traders.""")
 
 
 
